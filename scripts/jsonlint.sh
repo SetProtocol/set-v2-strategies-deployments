d6c4faf290d66aa47dd9a61d0d5a42bea034fbed
cgewecke <christophergewecke@gmail.com>
2022-01-10 14:43:50 -0800

    json lint optimism deployments

--- scripts/jsonlint.sh
+++ scripts/jsonlint.sh
@@ -11,3 +11,7 @@ echo "> ethereum/42-staging";    jsonlint -q ethereum/deployments/outputs/42-sta
 echo "> polygon/137-production";   jsonlint -q polygon/deployments/outputs/137-production.json
 echo "> polygon/137-staging";      jsonlint -q polygon/deployments/outputs/137-staging.json
 echo "> polygon/80001-production"; jsonlint -q polygon/deployments/outputs/80001-staging.json
+
+echo "> optimism/10-production";   jsonlint -q optimism/deployments/outputs/10-production.json
+echo "> optimism/10-staging";      jsonlint -q optimism/deployments/outputs/10-staging.json
+echo "> optimism/69-staging";      jsonlint -q optimism/deployments/outputs/69-staging.json
