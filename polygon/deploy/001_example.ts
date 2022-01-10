cd97eede21a49ac10426bdb307c00b1b65d007c7
cgewecke <christophergewecke@gmail.com>
2022-01-10 15:39:43 -0800

    Fix deploy scripts

--- polygon/deploy/001_example.ts
+++ polygon/deploy/001_example.ts
@@ -1,2 +1,20 @@
-export {}
+import "module-alias/register";
 
+import { HardhatRuntimeEnvironment as HRE } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+import {
+  getCurrentStage,
+} from "@utils/outputHelper";
+
+import { stageAlreadyFinished, trackFinishedStage, prepareDeployment } from "@utils/deploys/deployUtils";
+
+const CURRENT_STAGE = getCurrentStage(__filename);
+
+const func: DeployFunction = trackFinishedStage(CURRENT_STAGE, async function (hre: HRE) {
+  await prepareDeployment(hre);
+});
+
+func.skip = stageAlreadyFinished(CURRENT_STAGE);
+
+export default func;
